29605913195087b0ce8290adc82987bbcd2f31a3
Adirafi-22 <adirafi22@gmail.com>
2025-03-13 15:59:47 +0700

    main.c

--- main.c
+++ main.c
@@ -10,6 +10,15 @@ int main() {
     printf("Masukan bilangan 11 sebagai elemen kedua {insert last}\n");
     insertLast(11);
     
+    printf("Masukan bilangan 9 diantara bilangan 7 dan 11 {insert after}\n");
+    insertAfter(7, 9);
+
+    printf("masukan bilangan 5 di awal list {insert first}\n");
+    insertFirst(5);
+
+    printf("Masukan bilangan 13 di akhir list {insert last}\n");
+    insertLast(13);
+    
     return 0;
 }
 
