53be5840616e6db402d55eae9d1d274c0d23aba2
Adirafi-22 <adirafi22@gmail.com>
2025-03-13 15:48:16 +0700

    modul.c

--- modul.c
+++ modul.c
@@ -56,45 +56,5 @@ void insertLast(int value) {
     printElement(value);
 }
 
-// Insert After
-void insertAfter(int afterValue, int value) {
-    Node *temp = First;
-    while (temp != NULL && temp->info != afterValue) {
-        temp = temp->next;
-    }
-    if (temp == NULL) {
-        printf("Nilai %d tidak ditemukan dalam list!\n", afterValue);
-        return;
-    }
-    Node *newNode = createNode(value);
-    if (newNode == NULL) {
-        printf("Alokasi Memori Gagal!\n");
-        return;
-    }
-    newNode->next = temp->next;
-    temp->next = newNode;
-    printElement(value);
-}
 
-// Delete First
-void deleteFirst() {
-    if (First == NULL) {
-        printf("List kosong!\n");
-        return;
-    }
-    Node *temp = First;
-    int deletedValue = temp->info;
-    First = First->next;
-    free(temp);
-    printElement(deletedValue);
-}
-
-
-// Delete All
-void deleteAll() {
-    while (First != NULL) {
-        deleteFirst();
-    }
-    printf("List kosong!\n");
-}
 
